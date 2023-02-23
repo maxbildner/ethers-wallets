cb4a402dbaa0368852c01a055f8fd9778def74c1
maxbildner <39280058+maxbildner@users.noreply.github.com>
2023-02-23 15:55:18 -0500

    downgrade ethers library to v5.4

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+require("dotenv").config();
+const { ethers } = require("ethers");
 
+// let keys = Object.keys(ethers);
+// keys.sort();
+// keys.forEach((k) => console.log(k));
 
+const randomBytes = ethers.utils.randomBytes(16); // object- array of 16 numbers
+// console.log(randomBytes, typeof randomBytes);
+
+// Create a random mnemonic to use as the seed for the HDNode
+// const mnemonic = ethers.utils.entropyToMnemonic(randomBytes); // string- 12 words separated by spaces
+console.log(process.env.mnemonic);
